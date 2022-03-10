7ed5ec63997db7586b5250b6f8fad27be4338ca4
clxmente <solorioeclem164@gmail.com>
2022-03-10 00:39:14 -0800

    working countdown button with disables/enables working !

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js, jsx}",
     "./components/**/*.{js, jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
