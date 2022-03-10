161f250c6f53d3a52c33f936cce41d1a388d6a17
clxmente <solorioeclem164@gmail.com>
2022-03-10 15:55:09 -0800

    working switch cards to team on dropdown click, and player name enter

---
+++ components/Dropdown.jsx
@@ -0,0 +1,78 @@
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Dropdown(props) {
+
+  return (
+    <Listbox value={props.selected} onChange={ props.setSelected }>
+      {({ open }) => (
+        <>
+          <Listbox.Label className="block text-sm font-medium text-gray-700">Teams</Listbox.Label>
+          <div className="mt-1 relative w-full md:w-1/2 lg:w-2/12">
+            <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-0 sm:text-sm">
+              <span className="flex items-center">
+                <img src={props.selected.avatar} alt="" className="flex-shrink-0 h-6 w-6 rounded-full" />
+                <span className="ml-3 block truncate">{props.selected.name}</span>
+              </span>
+              <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                {props.teams.map((team) => (
+                  <Listbox.Option
+                    key={team.id}
+                    className={({ active }) =>
+                      classNames(
+                        active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                        'cursor-default select-none relative py-2 pl-3 pr-9'
+                      )
+                    }
+                    value={team}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <img src={team.avatar} alt="" className="flex-shrink-0 h-6 w-6 rounded-full" />
+                          <span
+                            className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
+                          >
+                            {team.name}
+                          </span>
+                        </div>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? 'text-white' : 'text-indigo-600',
+                              'absolute inset-y-0 right-0 flex items-center pr-4'
+                            )}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,220 @@ import { useState } from 'react';
 import Countdown from 'react-countdown';
 import PlayerBox from '../components/PlayerBox';
 import Data from "../data/players.json"; // player data json file
+import Dropdown from '../components/Dropdown';
+
+const teams = [
+  {
+    id: 1,
+    name: 'Atlanta Hawks',
+    abbreviation: "ATL",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612737/primary/L/logo.svg',
+  },
+  {
+    id: 2,
+    name: 'Boston Celtics',
+    abbreviation: "BOS",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg',
+  },
+  {
+    id: 3,
+    name: 'Brooklyn Nets',
+    abbreviation: "BKN",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612751/primary/L/logo.svg',
+  },
+  {
+    id: 4,
+    name: 'Charlotte Hornets',
+    abbreviation: "CHA",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612766/primary/L/logo.svg',
+  },
+  {
+    id: 5,
+    name: 'Chicago Bulls',
+    abbreviation: "CHI",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612741/primary/L/logo.svg',
+  },
+  {
+    id: 6,
+    name: 'Cleveland Cavaliers',
+    abbreviation: "CLE",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612739/primary/L/logo.svg',
+  },
+  {
+    id: 7,
+    name: 'Dallas Mavericks',
+    abbreviation: "DAL",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612742/primary/L/logo.svg',
+  },
+  {
+    id: 8,
+    name: 'Denver Nuggets',
+    abbreviation: "DEN",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612743/primary/L/logo.svg',
+  },
+  {
+    id: 9,
+    name: 'Detroit Pistons',
+    abbreviation: "DET",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612765/primary/L/logo.svg',
+  },
+  {
+    id: 10,
+    name: 'Golden State Warriors',
+    abbreviation: "GSW",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612744/primary/L/logo.svg',
+  },
+  {
+    id: 11,
+    name: 'Houston Rockets',
+    abbreviation: "HOU",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612745/primary/L/logo.svg',
+  },
+  {
+    id: 12,
+    name: 'Indiana Pacers',
+    abbreviation: "IND",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg',
+  },
+  {
+    id: 13,
+    name: 'Los Angeles Clippers',
+    abbreviation: "LAC",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612746/primary/L/logo.svg',
+  },
+  {
+    id: 14,
+    name: 'Los Angeles Lakers',
+    abbreviation: "LAL",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612747/primary/L/logo.svg',
+  },
+  {
+    id: 15,
+    name: 'Memphis Grizzlies',
+    abbreviation: "MEM",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612763/primary/L/logo.svg',
+  },
+  {
+    id: 16,
+    name: 'Miami Heat',
+    abbreviation: "MIA",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612748/primary/L/logo.svg',
+  },
+  {
+    id: 17,
+    name: 'Milwaukee Bucks',
+    abbreviation: "MIL",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612749/primary/L/logo.svg',
+  },
+  {
+    id: 18,
+    name: 'Minnesota Timberwolves',
+    abbreviation: "MIN",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg',
+  },
+  {
+    id: 19,
+    name: 'New Orleans Pelicans',
+    abbreviation: "NOP",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612740/primary/L/logo.svg',
+  },
+  {
+    id: 20,
+    name: 'New York Knicks',
+    abbreviation: "NYK",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg',
+  },
+  {
+    id: 21,
+    name: 'Oklahoma City Thunder',
+    abbreviation: "OKC",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg',
+  },
+  {
+    id: 22,
+    name: 'Orlando Magic',
+    abbreviation: "ORL",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612753/primary/L/logo.svg',
+  },
+  {
+    id: 23,
+    name: 'Philadelphia 76ers',
+    abbreviation: "PHI",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612755/primary/L/logo.svg',
+  },
+  {
+    id: 24,
+    name: 'Phoenix Suns',
+    abbreviation: "PHX",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612756/primary/L/logo.svg',
+  },
+  {
+    id: 25,
+    name: 'Portland Trail Blazers',
+    abbreviation: "POR",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612757/primary/L/logo.svg',
+  },
+  {
+    id: 26,
+    name: 'Sacramento Kings',
+    abbreviation: "SAC",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612758/primary/L/logo.svg',
+  },
+  {
+    id: 27,
+    name: 'San Antonio Spurs',
+    abbreviation: "SAS",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612759/primary/L/logo.svg',
+  },
+  {
+    id: 28,
+    name: 'Toronto Raptors',
+    abbreviation: "TOR",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612761/primary/L/logo.svg',
+  },
+  {
+    id: 29,
+    name: 'Utah Jazz',
+    abbreviation: "UTA",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612762/primary/L/logo.svg',
+  },
+  {
+    id: 30,
+    name: 'Washington Wizards',
+    abbreviation: "WAS",
+    avatar:
+      'https://cdn.nba.com/logos/nba/1610612764/primary/L/logo.svg',
+  },
+]
 
 const score = 0;
 
@@ -15,7 +229,7 @@ export default function Home() {
 
   // Team Selection States
   const [currTeam, setCurrTeam] = useState("ATL");
-  const [playersArr, setPlayersArr] = useState([]);
+  const [selected, setSelected] = useState(teams[0]);
 
   // Start/Timer Button States
   const [timerVal, setTimerVal] = useState("Start Game");
@@ -41,8 +255,8 @@ export default function Home() {
         res => {
             console.log(res.status)
             if (res.status === 200) { 
-                score++;
-                if (res.data[0].TEAM != currTeam) { setCurrTeam(res.data[0].TEAM) } // switch boxes to team if player correct and on another team
+              score++;
+              if (res.data[0].TEAM != currTeam) { setSelected(teams.find(abbrev => abbrev.abbreviation === res.data[0].TEAM)); setCurrTeam(res.data[0].TEAM); } // switch boxes to team if player correct and on another team
             }
         }
     ).catch(err => { console.log(err); })
@@ -64,7 +278,7 @@ export default function Home() {
 
   // Players map
   // TODO: Convert this to use API calls instead of data.json
-  const players_arr = Data.filter(player => player.TEAM===currTeam).map((data, id) => { // filter the whole player array data to players with team == currTeam
+  const players_arr = Data.filter(player => player.TEAM===selected.abbreviation).map((data, id) => { // filter the whole player array data to players with team == currTeam
     if (data.JERSEY_NUM) {
       var jersey_no = "#" + data.JERSEY_NUM;
     } else { jersey_no = "-"}
@@ -125,6 +339,8 @@ export default function Home() {
         </div>
         {/* End Input/Submit */}
 
+        {/* dropdown */}
+        <div className='px-5'><Dropdown selected={selected} setSelected={setSelected} teams={teams} /></div>
         {/* Player Boxes */}
         <div className="py-10 grid grid-cols-1 md:grid-cols-5 gap-4 px-5">
           {players_arr}
