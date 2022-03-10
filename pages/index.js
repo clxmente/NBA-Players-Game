0f259d3de475c10f678b040d4fe4342217c962ac
clxmente <solorioeclem164@gmail.com>
2022-03-10 01:26:56 -0800

    playerbox component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { useState } from 'react'
 import Countdown from 'react-countdown';
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [name, setName] = useState("");
