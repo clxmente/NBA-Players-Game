72e8d8b7a36afcc044d4d0391eeca5e671cdb202
clxmente <solorioeclem164@gmail.com>
2022-03-12 18:08:04 -0800

    [NOT WORKING] change guessedPlayers state to array of objs

--- components/PlayerBox.jsx
+++ components/PlayerBox.jsx
@@ -6,8 +6,7 @@ function PlayerBox(props) {
     <div>
       <div className="mt-1 block rounded-md">
         <input type={"text"} name={"player-name"} id={props.name.toLowerCase()} 
-          className="flex-1 block w-full px-3 py-2 focus:ring-0 sm:text-sm border-gray-300 rounded-tl-md rounded-tr-md focus:border-red-400"
-          value={props.name} readOnly
+          className="flex-1 block w-full px-3 py-2 focus:ring-0 sm:text-sm border-gray-300 rounded-tl-md rounded-tr-md focus:border-red-400" readOnly
         />
         <div className="flex">
           <span className="shadow-md inline-flex items-center px-3 py-2 border border-t-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm w-1/3 rounded-bl-md">
