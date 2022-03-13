872c4868f5133eae88ce46ed367778baa46ec97f
clxmente <solorioeclem164@gmail.com>
2022-03-13 14:09:56 -0700

    dark guessed boxes + turned off glow

--- components/GuessedPlayerBox.jsx
+++ components/GuessedPlayerBox.jsx
@@ -2,12 +2,12 @@ export default function GuessedPlayerBox(props) {
   return (
     <div className="relative">
       {/* background blur div */}
-      <div className="absolute inset-0 bg-green-400 rounded-lg blur-sm"></div>
+      {/* <div className="absolute inset-0 bg-white rounded-lg blur-sm"></div> */}
 
-      <div className="relative rounded-lg border border-white bg-white px-6 py-5 shadow-sm flex items-center space-x-3">
+      <div className="relative rounded-lg border border-gray-700 bg-gray-800 px-5 py-4 shadow-sm flex items-center space-x-3">
         <div className="flex-1 min-w-0">
           <span className="absolute inset-0" aria-hidden="true" />
-          <p className="text-sm font-medium text-gray-900">{props.person}</p>
+          <p className="text-sm font-medium text-white">{props.person}</p>
         </div>
       </div>
     </div>
