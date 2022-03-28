2e12c10dc0cbe664abe033f978359e65750485c8
clxmente <solorioeclem164@gmail.com>
2022-03-28 15:31:43 -0700

    [NBA-2] mobile nav

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,16 +1,14 @@
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
 import {
   GlobeIcon,
   EmojiHappyIcon,
   FireIcon,
   MenuIcon,
-  ShieldCheckIcon,
-  ChartBarIcon,
   XIcon,
-} from '@heroicons/react/outline'
-import { ChevronDownIcon, HomeIcon } from '@heroicons/react/solid'
-import Link from 'next/link'
+} from '@heroicons/react/outline';
+import { ChevronDownIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
 
 const diff_colors = {
   0: "text-green-400",
@@ -22,35 +20,35 @@ const game_diffs = [
   {
     name: 'Easy Mode',
     description: 'See Player Images, Jersey Numbers, and Positions when playing.',
-    href: '#',
+    href: '/easy',
     icon: EmojiHappyIcon,
   },
   {
     name: 'Normal Mode',
     description: 'Only Jersey Numbers and Positions will be shown when playing. NO Player Images.',
-    href: '#',
+    href: '/',
     icon: FireIcon,
   },
-  { name: 'Hard Mode', description: "NO Images, Positions, or Jersey Numbers.", href: '#', icon: FireIcon },
+  { name: 'Hard Mode', description: "NO Images, Positions, or Jersey Numbers.", href: '/hard', icon: FireIcon },
 ]
 
 const leaderboard_items = [
   {
-    name: 'Leaderboard Easy Mode',
+    name: 'Global Rankings: Easy Mode',
     description: 'View all the scores for easy mode.',
-    href: '#',
+    href: '/leaderboards/easy',
     icon: GlobeIcon,
   },
   {
-    name: 'Leaderboard Normal Mode',
+    name: 'Global Rankings: Normal Mode',
     description: 'View all the scores for normal mode.',
-    href: '#',
+    href: '/leaderboards/normal',
     icon: GlobeIcon,
   },
   {
-    name: 'Leaderboard Hard Mode',
+    name: 'Global Rankings: Hard Mode',
     description: 'View all the scores for hard mode.',
-    href: '#',
+    href: 'leaderboards/hard',
     icon: GlobeIcon,
   }
 ]
@@ -87,15 +85,15 @@ export default function NavBar() {
                 <>
                   <Popover.Button
                     className={classNames(
-                      open ? 'text-indigo-500' : 'text-gray-400',
-                      'group bg-gray-900 rounded-md inline-flex items-center text-base font-medium hover:text-gray-500 focus:outline-none focus:ring-none'
+                      open ? 'text-indigo-500' : 'text-white',
+                      'group bg-gray-900 rounded-md inline-flex items-center text-base font-medium hover:text-gray-400 focus:outline-none focus:ring-none'
                     )}
                   >
                     <span>Games</span>
                     <ChevronDownIcon
                       className={classNames(
-                        open ? 'text-indigo-500' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                        open ? 'text-indigo-500' : 'text-white',
+                        'ml-2 h-5 w-5 group-hover:text-gray-400'
                       )}
                       aria-hidden="true"
                     />
@@ -114,17 +112,15 @@ export default function NavBar() {
                       <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                         <div className="relative grid gap-6 bg-gray-700 px-5 py-6 sm:gap-8 sm:p-8">
                           {game_diffs.map((item, index) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800"
-                            >
-                              <item.icon className={`flex-shrink-0 h-6 w-6 ${diff_colors[index]}`} aria-hidden="true" />
-                              <div className="ml-4">
-                                <p className="text-base font-medium text-gray-200">{item.name}</p>
-                                <p className="mt-1 text-sm text-gray-400">{item.description}</p>
-                              </div>
-                            </a>
+                            <Link key={item.name} href={item.href} passHref >
+                              <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800">
+                                <item.icon className={`flex-shrink-0 h-6 w-6 ${diff_colors[index]}`} aria-hidden="true" />
+                                <div className="ml-4">
+                                  <p className="text-base font-medium text-gray-200">{item.name}</p>
+                                  <p className="mt-1 text-sm text-gray-400">{item.description}</p>
+                                </div>
+                              </a>
+                            </Link>
                           ))}
                         </div>
                       </div>
@@ -140,15 +136,15 @@ export default function NavBar() {
                 <>
                   <Popover.Button
                     className={classNames(
-                      open ? 'text-indigo-500' : 'text-gray-500',
-                      'group bg-gray-900 rounded-md inline-flex items-center text-base font-medium hover:text-gray-600 focus:outline-none focus:ring-none'
+                      open ? 'text-indigo-500' : 'text-white',
+                      'group bg-gray-900 rounded-md inline-flex items-center text-base font-medium hover:text-gray-400 focus:outline-none focus:ring-none'
                     )}
                   >
                     <span>Leaderboards</span>
                     <ChevronDownIcon
                       className={classNames(
-                        open ? 'text-indigo-500' : 'text-gray-500',
-                        'ml-2 h-5 w-5 group-hover:text-gray-600'
+                        open ? 'text-indigo-500' : 'text-white',
+                        'ml-2 h-5 w-5 group-hover:text-gray-400'
                       )}
                       aria-hidden="true"
                     />
@@ -167,17 +163,15 @@ export default function NavBar() {
                       <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                         <div className="relative grid gap-6 bg-gray-700 px-5 py-6 sm:gap-8 sm:p-8">
                           {leaderboard_items.map((item, index) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800"
-                            >
-                              <item.icon className={`flex-shrink-0 h-6 w-6 ${diff_colors[index]}`} aria-hidden="true" />
-                              <div className="ml-4">
-                                <p className="text-base font-medium text-gray-200">{item.name}</p>
-                                <p className="mt-1 text-sm text-gray-400">{item.description}</p>
-                              </div>
-                            </a>
+                            <Link key={item.name} href={item.href} passHref >
+                              <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800">
+                                <item.icon className={`flex-shrink-0 h-6 w-6 ${diff_colors[index]}`} aria-hidden="true" />
+                                <div className="ml-4">
+                                  <p className="text-base font-medium text-gray-200">{item.name}</p>
+                                  <p className="mt-1 text-sm text-gray-400">{item.description}</p>
+                                </div>
+                              </a>
+                            </Link>
                           ))}
                         </div>
                       </div>
@@ -204,8 +198,8 @@ export default function NavBar() {
         leaveFrom="opacity-100 scale-100"
         leaveTo="opacity-0 scale-95"
       >
-        <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+        <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 z-10 transition transform origin-top-right md:hidden">
+          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-gray-700 divide-y-2 divide-gray-600">
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
                 <div>
@@ -216,7 +210,7 @@ export default function NavBar() {
                   />
                 </div>
                 <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Popover.Button className="bg-gray-700 rounded-md p-2 inline-flex items-center justify-center text-white hover:text-gray-400 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Close menu</span>
                     <XIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
@@ -224,51 +218,31 @@ export default function NavBar() {
               </div>
               <div className="mt-6">
                 <nav className="grid gap-y-8">
-                  {game_diffs.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                    >
-                      <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
-                    </a>
+                  {game_diffs.map((item, index) => (
+                    <Link key={item.name} href={item.href} passHref>
+                      <a className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                        <item.icon className={`flex-shrink-0 h-6 w-6 ${diff_colors[index]}`} aria-hidden="true" />
+                        <span className="ml-3 text-base font-medium text-gray-200">{item.name}</span>
+                      </a>
+                    </Link>
                   ))}
                 </nav>
               </div>
             </div>
             <div className="py-6 px-5 space-y-6">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                  Pricing
-                </a>
-
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                  Docs
-                </a>
                 {leaderboard_items.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    {item.name}
-                  </a>
+                  <Link key={item.name} href={item.href} passHref>
+                    <a className="text-base font-medium text-gray-200 hover:text-gray-400">
+                      {item.name}
+                    </a>
+                  </Link>
                 ))}
-              </div>
-              <div>
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                    Sign in
+                <Link href={"https://solorio.dev/"} passHref>
+                  <a className="text-base font-medium text-gray-200 hover:text-gray-400">
+                    About the developer
                   </a>
-                </p>
+                </Link>
               </div>
             </div>
           </div>
